a0b03ed9ace9113e1d1f411fb990929320a6c91c
Kylan Hurt <kylan@datagoniaweb.com>
2016-11-14 21:34:58 -0800

    Rename Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+
+    less: {
+        files: {
+          'dist/css/main.css': 'src/less/main.less',
+          'dist/css/bootstrap.css': 'node_modules/bootstrap/less/bootstrap.less'
+
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.registerTask('default', ['jshint']);
+
+};
